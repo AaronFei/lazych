1bd4c66a60ccf9b7063244253880836576ddff08
Aaron Fei <afei@marvell.com>
2022-05-28 16:31:03 +0800

    First release

--- lazych.go
+++ lazych.go
@@ -1,19 +1,75 @@
 package lazych
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"sync"
+)
+
 type channel_t[T any] struct {
 	C chan T
 }
 
-var channels = map[string]interface{}{}
+var (
+	channels = map[string]interface{}{}
+	mutex    = sync.Mutex{}
+)
 
-func GetChannel[T any](name string) chan T {
+// Get the channel[T] with the name.
+// If the channel does not exist, will create the channel[T] with size.
+// Return error if the [T] is not same as the recorded [T].
+func GetCh[T any](name string, size int) (chan T, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	if _, ok := channels[name]; ok {
-		return channels[name].(channel_t[T]).C
+		t := reflect.TypeOf(channels[name])
+		t2 := reflect.TypeOf(channel_t[T]{})
+		if t.Name() == t2.Name() {
+			return channels[name].(channel_t[T]).C, nil
+		} else {
+			startIndex := strings.Index(t.Name(), "[") + 1
+			return nil, fmt.Errorf("Required T is \"%s\" but get \"%s\"", t.Name()[startIndex:len(t.Name())-1], t2.Name()[startIndex:len(t2.Name())-1])
+		}
+	} else {
+		ch := make(chan T, size)
+		channelInfo := channel_t[T]{
+			C: ch,
+		}
+		channels[name] = channelInfo
+		return channelInfo.C, nil
 	}
-	ch := make(chan T)
-	channelInfo := channel_t[T]{
-		C: ch,
+}
+
+// Delete the channel[T] with the name.
+// If the channel does not exist, will create the channel[T] with size.
+// Return error if the [T] is not same as the recorded [T] or the name does not exist.
+func DelCh[T any](name string) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if _, ok := channels[name]; ok {
+		t := reflect.TypeOf(channels[name])
+		t2 := reflect.TypeOf(channel_t[T]{})
+		if t.Name() == t2.Name() {
+			close(channels[name].(channel_t[T]).C)
+			delete(channels, name)
+			return nil
+		} else {
+			startIndex := len("channel_t[")
+			return fmt.Errorf("Required T is \"%s\" but get \"%s\"", t.Name()[startIndex:len(t.Name())-1], t2.Name()[startIndex:len(t2.Name())-1])
+		}
+	} else {
+		return fmt.Errorf("Channel %s does not exist", name)
 	}
-	channels[name] = channelInfo
-	return channelInfo.C
+}
+
+// Check the channel is exist or not.
+func IsChExist(name string) bool {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	_, ok := channels[name]
+	return ok
 }
