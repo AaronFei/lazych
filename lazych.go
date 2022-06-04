29714f3928f0ef8c576594c7f7c4dd1b65a3dfa5
Aaron Fei <afei@marvell.com>
2022-06-04 13:55:23 +0800

    Add requst/response ch

--- lazych.go
+++ lazych.go
@@ -11,17 +11,25 @@ type channel_t[T any] struct {
 	C chan T
 }
 
+type rrChannel_t[T any] struct {
+	Request  chan T
+	Response chan T
+}
+
 var (
-	channels = map[string]interface{}{}
-	mutex    = sync.Mutex{}
+	channels      = map[string]interface{}{}
+	channelsMutex = sync.Mutex{}
+
+	rrChannels      = map[string]interface{}{}
+	rrChannelsMutex = sync.Mutex{}
 )
 
 // Get the channel[T] with the name.
 // If the channel does not exist, will create the channel[T] with size.
 // Return error if the [T] is not same as the recorded [T].
 func GetCh[T any](name string, size int) (chan T, error) {
-	mutex.Lock()
-	defer mutex.Unlock()
+	channelsMutex.Lock()
+	defer channelsMutex.Unlock()
 
 	if _, ok := channels[name]; ok {
 		t := reflect.TypeOf(channels[name])
@@ -42,12 +50,21 @@ func GetCh[T any](name string, size int) (chan T, error) {
 	}
 }
 
+// Must get channel
+func MustGetCh[T any](name string, size int) chan T {
+	if c, err := GetCh[T](name, size); err != nil {
+		panic(err)
+	} else {
+		return c
+	}
+}
+
 // Delete the channel[T] with the name.
 // If the channel does not exist, will create the channel[T] with size.
 // Return error if the [T] is not same as the recorded [T] or the name does not exist.
 func DelCh[T any](name string) error {
-	mutex.Lock()
-	defer mutex.Unlock()
+	channelsMutex.Lock()
+	defer channelsMutex.Unlock()
 
 	if _, ok := channels[name]; ok {
 		t := reflect.TypeOf(channels[name])
@@ -67,9 +84,78 @@ func DelCh[T any](name string) error {
 
 // Check the channel is exist or not.
 func IsChExist(name string) bool {
-	mutex.Lock()
-	defer mutex.Unlock()
+	channelsMutex.Lock()
+	defer channelsMutex.Unlock()
 
 	_, ok := channels[name]
+	return ok
+}
+
+// Get the rrChannel[T] with the name.
+// If the RR channel does not exist, will create the rrChannel[T] with size.
+// Return error if the [T] is not same as the recorded [T].
+func GetRRCh[T any](name string, size int) (chan T, chan T, error) {
+	rrChannelsMutex.Lock()
+	defer rrChannelsMutex.Unlock()
+
+	if _, ok := rrChannels[name]; ok {
+		t := reflect.TypeOf(rrChannels[name])
+		t2 := reflect.TypeOf(rrChannel_t[T]{})
+		fmt.Println(t.Name(), t2.Name())
+		if t.Name() == t2.Name() {
+			return rrChannels[name].(rrChannel_t[T]).Request, rrChannels[name].(rrChannel_t[T]).Response, nil
+		} else {
+			startIndex := strings.Index(t.Name(), "[") + 1
+			return nil, nil, fmt.Errorf("Required T is \"%s\" but get \"%s\"", t.Name()[startIndex:len(t.Name())-1], t2.Name()[startIndex:len(t2.Name())-1])
+		}
+	} else {
+		channelInfo := rrChannel_t[T]{
+			Request:  make(chan T, size),
+			Response: make(chan T, size),
+		}
+		rrChannels[name] = channelInfo
+		return channelInfo.Request, channelInfo.Response, nil
+	}
+}
+
+// Must get RR channel
+func MustGetRRCh[T any](name string, size int) (chan T, chan T) {
+	if rq, rp, err := GetRRCh[T](name, size); err != nil {
+		panic(err)
+	} else {
+		return rq, rp
+	}
+}
+
+// Delete the rrChannel[T] with the name.
+// If the RR channel does not exist, will create the rrChannel[T] with size.
+// Return error if the [T] is not same as the recorded [T] or the name does not exist.
+func DelRRCh[T any](name string) error {
+	rrChannelsMutex.Lock()
+	defer rrChannelsMutex.Unlock()
+
+	if _, ok := rrChannels[name]; ok {
+		t := reflect.TypeOf(rrChannels[name])
+		t2 := reflect.TypeOf(rrChannel_t[T]{})
+		if t.Name() == t2.Name() {
+			close(rrChannels[name].(rrChannel_t[T]).Request)
+			close(rrChannels[name].(rrChannel_t[T]).Response)
+			delete(rrChannels, name)
+			return nil
+		} else {
+			startIndex := len("rrChannel_t[")
+			return fmt.Errorf("Required T is \"%s\" but get \"%s\"", t.Name()[startIndex:len(t.Name())-1], t2.Name()[startIndex:len(t2.Name())-1])
+		}
+	} else {
+		return fmt.Errorf("RR Channel %s does not exist", name)
+	}
+}
+
+// Check the RR channel is exist or not.
+func IsRRChExist(name string) bool {
+	rrChannelsMutex.Lock()
+	defer rrChannelsMutex.Unlock()
+
+	_, ok := rrChannels[name]
 	return ok
 }
